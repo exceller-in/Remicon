9228a013fced29f1273ce1e019e6f9dbfcdfb594
exceller-in <88068048+exceller-in@users.noreply.github.com>
2023-08-03 10:28:08 +0900

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,34 +2,14 @@ import numpy as np
 import altair as alt
 import pandas as pd
 import streamlit as st
+import os
 
-st.header('st.write')
+st.header('동양 레미콘 출하일보')
 
-# Example 1
+st.write('2023-08-03 :sunglasses:')
 
-st.write('Hello, *World!* :sunglasses:')
+df=pd.read_csv('./df.csv', encoding='EUC-KR')
 
-# Example 2
-
-st.write(1234)
-
-# Example 3
-
-df = pd.DataFrame({
-     'first column': [1, 2, 3, 4],
-     'second column': [10, 20, 30, 40]
-     })
 st.write(df)
 
-# Example 4
-
-st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
-
-# Example 5
-
-df2 = pd.DataFrame(
-     np.random.randn(200, 3),
-     columns=['a', 'b', 'c'])
-c = alt.Chart(df2).mark_circle().encode(
-     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-st.write(c)
+st.line_chart(df, x='index')
